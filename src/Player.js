f338506ae761910e571a78d26013fe6aaeb49438
Hebron George <hebrontgeorge@gmail.com>
2020-02-28 19:09:38 -0600

    Use the no cookie version of youtube player

--- src/Player.js
+++ src/Player.js
@@ -9,7 +9,8 @@ class Player extends React.Component {
       width:  '100%',
       playerVars: {
         autoplay: 1
-      }
+      },
+      host: "https://www.youtube-nocookie.com"
     };
 
     return(<Youtube videoId={this.props.videoId} opts={opts} onReady={this._onReady} onEnd={this.props.onEnd} />);
