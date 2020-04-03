4283362c11926f353f477767fe8d04b112cf510e
ras2 <ras2@live.com>
2020-04-02 23:16:26 -0500

    Formatting changes, removed unused code

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,36 +1,30 @@
 import React from 'react';
 
 class Checkbox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isChecked: false,
-        }
-        
-        this.toggleCheckboxChange = this.toggleCheckboxChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.toggleCheckboxChange = this.toggleCheckboxChange.bind(this);
+  }
 
-    toggleCheckboxChange() {
-        //this.setState({ isChecked: !this.state.isChecked });
-        this.props.handleCheckboxChange(this.props.value);
-    }
+  toggleCheckboxChange() {
+    this.props.handleCheckboxChange(this.props.value);
+  }
 
-    render() {
-
-        return (
-            <div className="checkbox">
-                <label>
-                    <input 
-                        type="checkbox"
-                        value={this.props.value}
-                        checked={this.props.checked}
-                        onChange={this.toggleCheckboxChange}
-                    />
-                    {this.props.label}
-                </label>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="checkbox">
+        <label>
+          <input 
+            type="checkbox"
+            value={this.props.value}
+            checked={this.props.checked}
+            onChange={this.toggleCheckboxChange}
+          />
+          {this.props.label}
+        </label>
+      </div>
+    )
+  }
 }
 
 export default Checkbox;
