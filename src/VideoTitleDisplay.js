0a8dcbd7d0abcc6b97ca7607fff4a33b78ad7e86
ras2 <ras2@live.com>
2020-04-23 22:43:50 -0500

    Change VideoTitleDisplay into function component

--- src/VideoTitleDisplay.js
+++ src/VideoTitleDisplay.js
@@ -1,43 +1,34 @@
 import React from 'react';
 
-class VideoTitleDisplay extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      url: `https://youtube.com/watch?v=${this.props.videoId}`
-    }
-    this.clickEvent = this.clickEvent.bind(this);
-  }
-
-  clickEvent(){
-    if (this.props.videoClicked) {
-      this.props.videoClicked(this.props.videoId);
+function VideoTitleDisplay(props) {
+  function clickEvent(){
+    if (props.videoClicked) {
+      props.videoClicked(props.videoId);
      } else {
       console.log('😎');
     }
   }
 
-  render() {
-    const titleOpts = {
-      flex: 1
-    }
-    const anchorOpts = {
-      paddingLeft: 3, 
-      flex: 0
-    }
-
-
-    return(
-      <div 
-        id="videoTitleDisplay" 
-        onClick={this.clickEvent}
-        className={`${this.props.className}${this.props.selected?' selected':''}`}
-      >
-        <div style={titleOpts}>{this.props.title}</div>
-        <a style={anchorOpts} href={this.state.url}><img alt='Go to Youtube' src={'/arrow-up-right.svg'}></img></a>
-      </div>
-      );
+  const titleOpts = {
+    flex: 1
   }
+  const anchorOpts = {
+    paddingLeft: 3, 
+    flex: 0
+  }
+
+  return (
+    <div 
+      id="videoTitleDisplay" 
+      onClick={() => clickEvent()}
+      className={`${props.className}${props.selected?' selected':''}`}
+    >
+      <div style={titleOpts}>{props.title}</div>
+      <a style={anchorOpts} href={`https://youtube.com/watch?v=${props.videoId}`}>
+        <img alt='Go to Youtube' src={'/arrow-up-right.svg'}></img>
+      </a>
+    </div>
+  );
 }
 
 export default VideoTitleDisplay;
