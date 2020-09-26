991b219a8dfbf7b42f3b9cf2bcf41339c55eb8f3
Hebron George <hebrontgeorge@gmail.com>
2020-09-25 21:10:20 -0500

    WIP

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -1,10 +1,11 @@
-// let APIRoot = "http://localhost:8000"; // When running locally
-let APIRoot = "https://happy-youtube-watcher.herokuapp.com";
+let APIRoot = "http://localhost:8000"; // When running locally
+// let APIRoot = "https://happy-youtube-watcher.herokuapp.com";
 let APIRootYoutube = "https://www.googleapis.com/youtube/v3";
 export default {
   APIEndpoints: {
     SHUFFLE: APIRoot + "/shuffle",
     TRACKED_PLAYLISTS: APIRoot + "/tracked-playlists",
     YOUTUBE_PLAYLISTS: APIRootYoutube + "/playlists",
+    TRACKED_VIDEOS:    APIRoot + "/videos",
   },
 };
--- src/pages/PlaylistsSearch.jsx
+++ src/pages/PlaylistsSearch.jsx
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import axios from 'axios';
+import AppConstants from '../AppConstants';
 import SearchField from 'react-search-field'
 import TypeChecker from 'typeco';
 
-const getMatchedList = (searchText) => {
+const List = props => (
+  <div className="list">
+    <div className="list-header">
+      <ul>
+        <li>Video ID</li>
+        <li>Title</li>
+        <li>Playlist ID</li>
+        <li>Playlist Name</li>
+        <li>Channel</li>
+        <li>Description</li>
+      </ul>
+    </div>
 
-};
+    <div className="list-body">
+      {
+        filteredList(props.queryString, props.list).map((item, index) => (
+          <ul key={index}>
+            {/*<li>{item.videoId}</li>*/}
+            <li>{item.title}</li>
+{/*            <li>{item.playlistId}</li>
+            <li>{item.playlistName}</li>
+            <li>{item.channelName}</li>
+            <li>{item.description}</li>*/}
+          </ul>
+        ))
+      }
+    </div>
+  </div>
+);
+
+function filteredList(queryString, list) {
+  // if (videoId, title, playlistId, playlistName, channelName, description)
+  //   has *queryString* 
+  return list.filter(item => matchingItem(item, queryString))
+}
+
+function matchingItem(item, queryString) {
+  console.log(`Filtering on list with queryString: ${queryString}`)
+  if (queryString === "") { return true } 
+  
+  if (item.videoId.toString().search(queryString) ||
+    item.title.toString().search(queryString)
+    ) {
+    console.log(`videoID or title includes ${queryString}`)
+    return true;
+  }
+}
+
+
 
 function PlaylistsSearch(props) {
+  const [loadedVideos, setLoadedVideos] = useState([]);
+  const [displayResults, setDisplayResults] = useState([]);
+  const [queryString, setQueryString] = useState("");
+  const getMatchedList = (searchText) => {
+    if (TypeChecker.isEmpty(searchText)) return loadedVideos;
+  };
+
+  const handleInput = value => {
+    setQueryString(value);
+  }
+
+  function loadVideos() {
+    axios.get(AppConstants.APIEndpoints.TRACKED_VIDEOS)
+    .then(response => setLoadedVideos(response.data))
+    .catch(error => console.log(`Couldn't retrieve tracked videos: ${error}`))
+  }
+
+  useEffect(loadVideos, []);
+
   return(
     <div>
-      <SearchField placeholder="Search Videos..." />
+      <SearchField onChange={handleInput} placeholder="Search Videos..." />
+      <List queryString={queryString} list={loadedVideos} />
     </div>
     );
 }
 
-export default PlaylistsSearch;
+export default PlaylistsSearch;  
