bad3461f9ae3842a367a5b01cb6449867d5ad1a4
Hebron George <hebrontgeorge@gmail.com>
2020-09-29 09:45:06 -0500

    Search on videoId, title, and description

--- src/pages/PlaylistsSearch.jsx
+++ src/pages/PlaylistsSearch.jsx
@@ -2,31 +2,24 @@ import React, { useState, useEffect }  from 'react';
 import axios from 'axios';
 import AppConstants from '../AppConstants';
 import SearchField from 'react-search-field'
-import TypeChecker from 'typeco';
 
 const List = props => (
   <div className="list">
     <div className="list-header">
-      <ul>
-        <li>Video ID</li>
-        <li>Title</li>
-        <li>Playlist ID</li>
-        <li>Playlist Name</li>
-        <li>Channel</li>
-        <li>Description</li>
-      </ul>
+      {/* once we decide on a table structure, we can fill out the headers here */}
     </div>
 
     <div className="list-body">
       {
         filteredList(props.queryString, props.list).map((item, index) => (
           <ul key={index}>
-            {/*<li>{item.videoId}</li>*/}
-            <li>{item.title}</li>
-{/*            <li>{item.playlistId}</li>
-            <li>{item.playlistName}</li>
-            <li>{item.channelName}</li>
-            <li>{item.description}</li>*/}
+            <li>
+              <a href={`https://youtube.com/watch?v=${item.videoId}`}
+                 target="_blank"
+                 rel="noopener noreferrer">
+                {item.title}
+              </a>
+            </li>
           </ul>
         ))
       }
@@ -35,17 +28,16 @@ const List = props => (
 );
 
 function filteredList(queryString, list) {
-  // if (videoId, title, playlistId, playlistName, channelName, description)
-  //   has *queryString* 
   return list.filter(item => matchingItem(item, queryString))
 }
 
 function matchingItem(item, queryString) {
   console.log(`Filtering on list with queryString: ${queryString}`)
   if (queryString === "") { return true } 
-  
-  if (item.videoId.toString().search(queryString) ||
-    item.title.toString().search(queryString)
+  queryString = queryString.toLowerCase();
+  if (item.videoId.toString().toLowerCase().search(queryString) >= 0 ||
+    item.title.toString().toLowerCase().search(queryString) >= 0 ||
+    item.description.toString().toLowerCase().search(queryString) >= 0
     ) {
     console.log(`videoID or title includes ${queryString}`)
     return true;
@@ -56,11 +48,7 @@ function matchingItem(item, queryString) {
 
 function PlaylistsSearch(props) {
   const [loadedVideos, setLoadedVideos] = useState([]);
-  const [displayResults, setDisplayResults] = useState([]);
   const [queryString, setQueryString] = useState("");
-  const getMatchedList = (searchText) => {
-    if (TypeChecker.isEmpty(searchText)) return loadedVideos;
-  };
 
   const handleInput = value => {
     setQueryString(value);
