6182cde2b8fdd77394d43d91fadc0922d6715926
Hebron George <hebrontgeorge@gmail.com>
2021-07-17 12:33:03 -0400

    Assign AppConstants to a variable before exporting as module default

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -1,9 +1,10 @@
 // let APIRoot = "http://localhost:8000"; // When running locally
 let APIRoot = "https://happy-youtube-watcher.herokuapp.com";
-export default {
+const AppConstants = {
   APIEndpoints: {
     SHUFFLE: APIRoot + "/shuffle",
     TRACKED_PLAYLISTS: APIRoot + "/tracked-playlists",
     TRACKED_VIDEOS:    APIRoot + "/videos",
   },
-};
+}
+export default AppConstants;
