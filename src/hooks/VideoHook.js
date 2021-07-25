e2e9065ff622e142c074e51ce7265ed326c6cd62
ras <ras2@live.com>
2021-07-24 22:35:42 -0500

    changes

--- src/hooks/VideoHook.js
+++ src/hooks/VideoHook.js
@@ -6,23 +6,29 @@ import {
 import axios from 'axios';
 import AppConstants from '../AppConstants';
 
+const initialReducedState = {
+  videos: [],
+  isLoaded: false, 
+  error: null,
+  isError: false,
+};
+
 const videoFetchReducer = (_, event) => {
   switch (event.type) {
     case 'FETCH_SUCCESS':
       return {
+        ...initialReducedState,
         videos: event.data.songs,
         isLoaded: true,
-        isError: false,
       };
     case 'FETCH_FAILURE':
       const reducedState = {
-        videos: [],
-        isLoaded: false,
+        ...initialReducedState,
         error: event.error,
         isError: true,
       };
       console.log("Error encountered during video fetch");
-      console.log(reducedState);
+      console.log(reducedState.error);
       return reducedState;
     default:
       throw new Error(`Unhandled event type: ${event.type}`);
@@ -43,11 +49,7 @@ const fetchData = async (url, requestBody, raiseEvent, isCancelled) => {
 export default function useVideoHook(initialPlaylistIds) {
   const [playlistIds, setPlaylistIds] = useState(initialPlaylistIds);
   const [isCancelled, setIsCancelled] = useState(false);
-  const [reducedState, setReducedState] = useReducer(videoFetchReducer, {
-    videos: [],
-    isLoaded: false, 
-    isError: false,
-  });
+  const [reducedState, setReducedState] = useReducer(videoFetchReducer, initialReducedState);
 
   useEffect(() => {
     fetchData(AppConstants.APIEndpoints.SHUFFLE, { playlist_ids: playlistIds }, setReducedState, isCancelled);
