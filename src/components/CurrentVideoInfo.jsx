f494db1a8436f38d3cf4c47145ea7fc762e7a584
Hebron George <hebrontgeorge@gmail.com>
2022-07-18 19:12:02 -0400

    Default description to collapsed when playing next video

--- src/components/CurrentVideoInfo.jsx
+++ src/components/CurrentVideoInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React from 'react';
 
 function CurrentVideoInfo(props) {
   const titleOpts = {
@@ -9,11 +9,9 @@ function CurrentVideoInfo(props) {
     flex: 0
   }
 
-  const [videoInfoCollapsed, setVideoInfoCollapsed] = useState(true);
-
   return(
     <>
-    <div id="videoTitleDisplay" className='currentVideoTitle contentRow' onClick={() => setVideoInfoCollapsed(!videoInfoCollapsed)}>
+    <div id="videoTitleDisplay" className='currentVideoTitle contentRow' onClick={() => props.setCollapseDescription(!props.collapseDescription)}>
       <div style={titleOpts}>{props.currentVideo.title}</div>
       <a style={anchorOpts} href={`https://youtube.com/watch?v=${props.currentVideo.video_id}`} target="_blank" rel="noopener noreferrer">
         <img alt='Go to Youtube' src={'/arrow-up-right.svg'}></img>
@@ -21,7 +19,7 @@ function CurrentVideoInfo(props) {
     </div>
 
     <div className="contentRow">
-      <div className={`${videoInfoCollapsed ? 'hide' : ''}`}>
+      <div className={`${props.collapseDescription ? 'hide' : ''}`}>
         {props.currentVideo.description}
       </div>
     </div>
