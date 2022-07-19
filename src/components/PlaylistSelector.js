c379d582330caf9a068852e6273ef58439d90d2c
ras1 <ras2@live.com>
2022-07-19 00:02:09 +0000

    move code to playlist selector

--- src/components/PlaylistSelector.js
+++ src/components/PlaylistSelector.js
@@ -3,9 +3,21 @@ import PlaylistSelectorItem from './PlaylistSelectorItem';
 
 function onSelectNone(props) {
   const selectedNoPlaylists = props.playlists.map(p => { 
-    return { ...p, is_default: false }});
+    return { ...p, is_default: false }
+  });
   props.setPlaylistIds([]);
   props.setLoadedPlaylists(selectedNoPlaylists);
+}
+
+function togglePlaylistSelection(props, togglePlaylistId) {
+  const toggledOnePlaylist = props.playlists.map(p => { 
+    return p.playlist_id === togglePlaylistId ? { ...p, is_default: !p.is_default } : p
+  });
+  const playlistIds = toggledOnePlaylist
+    .filter(p => p.is_default)
+    .map(p => p.playlist_id);
+  props.setPlaylistIds(playlistIds);
+  props.setLoadedPlaylists(toggledOnePlaylist);
 }
 
 function PlaylistSelector(props) {
@@ -19,7 +31,7 @@ function PlaylistSelector(props) {
           label={p.name}
           value={p.playlist_id}
           checked={p.is_default}
-          onCheckboxChange={(playlist_id) => props.onCheckboxChange(playlist_id)}
+          onCheckboxChange={(playlist_id) => togglePlaylistSelection(props, playlist_id)}
           />
         )}
       </div>
--- src/pages/ShufflePlayer.js
+++ src/pages/ShufflePlayer.js
@@ -2,47 +2,34 @@ import React, { useState, useEffect }  from 'react';
 import axios from 'axios';
 import AppConstants from '../AppConstants';
 import ButtonList from '../components/ButtonList';
+import CurrentVideoInfo from '../components/CurrentVideoInfo';
 import LoadingPlaceholder from '../components/LoadingPlaceholder';
+import PlayedHistory from '../components/PlayedHistory';
 import Player from '../components/Player';
 import PlaylistSelector from '../components/PlaylistSelector';
-import PlayedHistory from '../components/PlayedHistory';
-import CurrentVideoInfo from '../components/CurrentVideoInfo';
 import useVideoHook from '../hooks/VideoHook';
 
-function randomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 function ShufflePlayer() {
-  const [loadedPlaylists, setLoadedPlaylists] = useState([]);
-  const [currentVideo, setCurrentVideo] = useState({});
-  const [playedVideos, setPlayedVideos] = useState([]);
-  const [repeatVideo, setRepeatVideo] = useState(false);
-  const [hideVideo, setHideVideo] = useState(true);
+  const [loadedPlaylists,     setLoadedPlaylists] =     useState([]);
+  const [currentVideo,        setCurrentVideo] =        useState({});
+  const [playedVideos,        setPlayedVideos] =        useState([]);
+  const [repeatVideo,         setRepeatVideo] =         useState(false);
+  const [hideVideo,           setHideVideo] =           useState(true);
   const [collapseDescription, setCollapseDescription] = useState(true);
-  const [playlistIds, setPlaylistIds] = useState([]);
-  const [videoResult, reloadVideos] = useVideoHook(playlistIds);
+  const [playlistIds,         setPlaylistIds] =         useState([]);
+  const [videoResult, reloadVideos] =                   useVideoHook(playlistIds);
+  useEffect(loadPlaylists, []);
+  useEffect(pickNextVideo, [videoResult]);
 
   function loadPlaylists() {
-    axios
-      .get(AppConstants.APIEndpoints.TRACKED_PLAYLISTS)
+    axios.get(AppConstants.APIEndpoints.TRACKED_PLAYLISTS)
       .then(response => setLoadedPlaylists(response.data))
       .catch(error => console.log(`Couldn't retrieve tracked playlists! ${error}`))
   }
 
-  function togglePlaylistSelection(togglePlaylistId) {
-    const toggledOnePlaylist = loadedPlaylists.map(p => { 
-      return p.playlist_id === togglePlaylistId ? 
-        { ...p, is_default: !p.is_default } : p});
-    const playlistIds = toggledOnePlaylist
-      .filter(p => p.is_default)
-      .map(p => p.playlist_id);
-    setPlaylistIds(playlistIds);
-    setLoadedPlaylists(toggledOnePlaylist);
-  }
-
   function pickNextVideo() {
     if (!videoResult.isLoaded) { return; }
+    function randomInteger(min, max) { return Math.floor(Math.random() * (max - min)) + min; }
     const videoIndex = randomInteger(0, videoResult.videos.length);
     const nextVideo = videoResult.videos[videoIndex];
     playVideo(nextVideo);
@@ -53,10 +40,6 @@ function ShufflePlayer() {
     setCollapseDescription(true);
     setPlayedVideos(played => played.concat(video))
   }
-  
-
-  useEffect(loadPlaylists, []);
-  useEffect(pickNextVideo, [videoResult]);
 
   return ( !videoResult.isLoaded ? <LoadingPlaceholder /> : 
     <div>
@@ -72,7 +55,6 @@ function ShufflePlayer() {
       <div className='contentRow'>
         <PlaylistSelector
           playlists={loadedPlaylists}
-          onCheckboxChange={(playlist_id) => togglePlaylistSelection(playlist_id)}
           onShuffle={() => reloadVideos(playlistIds)}
           setPlaylistIds={setPlaylistIds}
           setLoadedPlaylists={setLoadedPlaylists}
