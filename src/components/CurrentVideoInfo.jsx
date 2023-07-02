8095473258bcf37e886ac86322acaeae5885fbbe
root <root@DESKTOP-9TDA9JT>
2023-07-01 22:53:10 -0500

    use Textfit lib

--- src/components/CurrentVideoInfo.jsx
+++ src/components/CurrentVideoInfo.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Textfit } from 'react-textfit';
 
 function CurrentVideoInfo(props) {
-  const titleOpts = {
-    flex: 1
+  var titleOpts = {
+    flex: 1,
+    height: "110px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center" 
   }
   const anchorOpts = {
     paddingLeft: 3, 
@@ -10,42 +15,10 @@ function CurrentVideoInfo(props) {
     flex: 0
   }
 
-  function fitText(outputSelector){
-    // max font size in pixels
-    const maxFontSize = 50;
-    // get the DOM output element by its selector
-    let outputDiv = document.getElementById(outputSelector);
-    // get element's width
-    let width = outputDiv.clientWidth;
-    // get content's width
-    let contentWidth = outputDiv.scrollWidth;
-    // get fontSize
-    let fontSize = parseInt(window.getComputedStyle(outputDiv, null).getPropertyValue('font-size'),10);
-    // if content's width is bigger then elements width - overflow
-    if (contentWidth > width){
-        fontSize = Math.ceil(fontSize * width/contentWidth,10);
-        fontSize =  fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize - 1;
-        outputDiv.style.fontSize = fontSize+'px';   
-    }else{
-        // content is smaller then width... let's resize in 1 px until it fits 
-        while (contentWidth === width && fontSize < maxFontSize){
-            fontSize = Math.ceil(fontSize) + 1;
-            fontSize = fontSize > maxFontSize  ? fontSize = maxFontSize  : fontSize;
-            outputDiv.style.fontSize = fontSize+'px';   
-            // update widths
-            width = outputDiv.clientWidth;
-            contentWidth = outputDiv.scrollWidth;
-            if (contentWidth > width){
-                outputDiv.style.fontSize = fontSize-1+'px'; 
-            }
-        }
-    }
-}
-
   return(
     <>
     <div id="videoTitleDisplay" className='currentVideoTitle' onClick={() => props.setCollapseDescription(!props.collapseDescription)}>
-      <div style={titleOpts}>{props.currentVideo.title}</div>
+      <Textfit style={titleOpts} mode="multi" max={44}>{props.currentVideo.title}</Textfit>
       <a style={anchorOpts} href={`https://youtube.com/watch?v=${props.currentVideo.video_id}`} target="_blank" rel="noopener noreferrer">
         <img alt='Go to Youtube' src={'/arrow-up-right.svg'}></img>
       </a>
--- src/hooks/VideoHook.js
+++ src/hooks/VideoHook.js
@@ -1,7 +1,4 @@
-import {
-  useState, 
-  useEffect,
-}  from 'react'
+import { useState, useEffect, }  from 'react'
 import axios from 'axios'
 import AppConstants from '../AppConstants'
 
