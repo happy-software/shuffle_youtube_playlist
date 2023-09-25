a7c466cf086931185726df6ad4527d45cbb24375
Robert Skinner <ras2@live.com>
2023-09-24 21:18:23 -0500

    Simplify ShufflePlayer

---
+++ src/hooks/UseEffectOnce.js
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from 'react';
+
+export default function useEffectOnce(effectFunction) {
+    const wasUsed = useRef(false)
+
+    useEffect(() => {
+        if (!wasUsed.current) {
+            effectFunction()
+            wasUsed.current = true
+        }
+    }, [effectFunction])
+}
--- src/pages/ShufflePlayer.js
+++ src/pages/ShufflePlayer.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import AppConstants from '../AppConstants';
 import ButtonList from '../components/ButtonList';
 import CurrentVideoInfo from '../components/CurrentVideoInfo';
 import LoadingPlaceholder from '../components/LoadingPlaceholder';
-import VideoSelector from '../components/VideoSelector';
 import Player from '../components/Player';
 import PlaylistSelector from '../components/PlaylistSelector';
-import useVideoHook from '../hooks/VideoHook';
+import VideoSelector from '../components/VideoSelector';
+import usePlaylistDataFetcher from '../hooks/UsePlaylistDataFetcher';
+import useVideoDataFetcher from '../hooks/UseVideoDataFetcher';
 
-function ShufflePlayer() {
-  const [playlists, setPlaylists] = useState([])
+export default function ShufflePlayer() {
   const [currentVideo, setCurrentVideo] = useState({})
   const [playedVideos, setPlayedVideos] = useState([])
   const [selectedPlaylistIds, setSelectedPlaylistIds] = useState(() => {
@@ -22,42 +20,19 @@ function ShufflePlayer() {
   const [hideVideo, setHideVideo] = useState(true)
   const [hideDescription, setHideDescription] = useState(true)
 
-  const [videosResult, fetchPlaylistVideos] = useVideoHook(selectedPlaylistIds)
-  useEffect(pickNextVideo, [videosResult])
+  const [playlists, setPlaylists] = usePlaylistDataFetcher(selectedPlaylistIds);
+
   useEffect(() => {
     localStorage.setItem("selectedPlaylistIds", JSON.stringify(selectedPlaylistIds));
   }, [selectedPlaylistIds]);
-
-  useCallbackOnce(loadPlaylists)
-
-  function useCallbackOnce(callbackFunction, condition = true) {
-    const isCalledRef = React.useRef(false);
-
-    React.useEffect(() => {
-      if (condition && !isCalledRef.current) {
-        isCalledRef.current = true;
-        callbackFunction();
-      }
-    }, [callbackFunction, condition]);
-  }
-
-  function loadPlaylists() {
-    axios.get(AppConstants.APIEndpoints.TRACKED_PLAYLISTS)
-      .then(response => {
-        response.data = response.data.map(item => ({
-          ...item,
-          is_default: selectedPlaylistIds.length > 0 ? selectedPlaylistIds.includes(item.playlist_id) : item.is_default
-        }))
-        setPlaylists(response.data)
-      })
-      .catch(error => console.log(`Couldn't retrieve tracked playlists! ${error}`))
-  }
+  const [videoFetchResult, setVideoFetchPlaylistIds] = useVideoDataFetcher()
+  useEffect(pickNextVideo, [videoFetchResult])
 
   function pickNextVideo() {
-    if (!videosResult.isLoaded) { return; }
+    if (!videoFetchResult.isLoaded) { return; }
     function randomInteger(min, max) { return Math.floor(Math.random() * (max - min)) + min; }
-    const videoIndex = randomInteger(0, videosResult.videos.length);
-    const nextVideo = videosResult.videos[videoIndex];
+    const videoIndex = randomInteger(0, videoFetchResult.videos.length);
+    const nextVideo = videoFetchResult.videos[videoIndex];
     playVideo(nextVideo);
   }
 
@@ -67,7 +42,7 @@ function ShufflePlayer() {
     setPlayedVideos(played => played.concat(video))
   }
 
-  return (!videosResult.isLoaded ? <LoadingPlaceholder /> :
+  return (!videoFetchResult.isLoaded ? <LoadingPlaceholder /> :
     <div>
       <Player
         videoId={currentVideo.video_id}
@@ -93,7 +68,7 @@ function ShufflePlayer() {
         <PlaylistSelector
           playlists={playlists}
           isCollapsedDefault={true}
-          onShuffle={() => fetchPlaylistVideos(selectedPlaylistIds)}
+          onShuffle={() => setVideoFetchPlaylistIds(selectedPlaylistIds)}
           setPlaylistIds={setSelectedPlaylistIds}
           setLoadedPlaylists={setPlaylists}
           className='playlistSelector'
@@ -107,5 +82,3 @@ function ShufflePlayer() {
     </div>
   )
 }
-
-export default ShufflePlayer;
