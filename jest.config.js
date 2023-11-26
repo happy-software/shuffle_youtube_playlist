69be34499db473795af16820e59e8c4aa1c1863a
Robert Skinner <ras2@live.com>
2023-11-25 23:11:48 -0600

    force axios import to resolve with CJS module

--- jest.config.js
+++ jest.config.js
@@ -74,14 +74,16 @@ module.exports = {
   moduleFileExtensions: [
     "js",
     "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
+    // "ts",
+    // "tsx",
+    // "json",
+    // "node"
   ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'),
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
