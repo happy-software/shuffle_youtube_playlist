b59c6dc332717b474885b6b64214cfa1ffd84e2d
Robert Skinner <ras2@live.com>
2024-08-04 13:13:30 -0400

    fix next button x2, fix onError

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 
 function Player(props) {
+  function onError() {
+    var internalPlayer = props.playerRef.current?.getInternalPlayer();
+    var videoUrl = internalPlayer.getVideoUrl()
+    if (!videoUrl) return;
+    console.log(`BROKEN VIDEO: ${getVideoId(videoUrl)}`);
+    internalPlayer.nextVideo();
+  }
+
+  function getVideoId(url) {
+    const urlParams = new URLSearchParams(new URL(url).search);
+    return urlParams.get('v');
+  }
+
   return (
     <div className='playerWrapper' style={{ display: props.hideVideo ? 'none' : 'block' }}>
       <ReactPlayer
@@ -13,6 +26,7 @@ function Player(props) {
         playing={true}
         width={"100%"}
         height={"800px"}
+        onError={onError}
         config={{
           youtube: {
             playerVars: {},
