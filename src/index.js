8f5b46a482b810891f3da6a84b8694f668248f99
ras1 <ras2@live.com>
2024-08-09 00:03:06 -0400

    Add Honeybadger Application Monitoring/Error Tracking (#113)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './normalize.css';
 import AppRoot from './AppRoot';
+import { Honeybadger, HoneybadgerErrorBoundary } from "@honeybadger-io/react"
 
-ReactDOM.render(<AppRoot />, document.getElementById("root"));
+const config = {
+    apiKey: process.env.REACT_APP_HONEYBADGER_API_KEY,
+    environment: "production"
+}
+
+const honeybadger = Honeybadger.configure(config)
+
+ReactDOM.render(<HoneybadgerErrorBoundary honeybadger={honeybadger}><AppRoot /></HoneybadgerErrorBoundary >, document.getElementById("root"));
