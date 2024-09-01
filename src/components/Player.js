c70e350b6301fd4f6fa79a0051e177e020b9f63c
ras1 <ras2@live.com>
2024-08-31 21:50:49 -0400

    Fix error with first video by creating a mechanism to refetch the current list, causing a reshuffle (#121)

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
+import { Honeybadger } from "@honeybadger-io/react"
 
 export default function Player(props) {
   const YOUTUBE_PLAYLIST_VIDEO_LIMIT = 200
 
   function onPlay() {
-    var internalPlayer = props.playerRef.current?.getInternalPlayer()
-    var videoData = internalPlayer.getVideoData()
-    var video = props.videos.find(v => v.video_id === videoData.video_id)
+    const internalPlayer = props.playerRef.current?.getInternalPlayer()
+    const videoData = internalPlayer.getVideoData()
+    const video = props.videos.find(v => v.video_id === videoData.video_id)
     props.setCurrentVideo(video)
   }
 
   function onError() {
-    var internalPlayer = props.playerRef.current?.getInternalPlayer()
-    var videoUrl = internalPlayer.getVideoUrl()
+    const internalPlayer = props.playerRef.current?.getInternalPlayer()
+    const videoUrl = internalPlayer.getVideoUrl()
     if (!videoUrl) return
-    console.log(`BROKEN VIDEO: ${getVideoId(videoUrl)}`)
-    internalPlayer.nextVideo()
+
+    const videoId = getVideoId(videoUrl)
+    const currentVideoIndex = internalPlayer.getPlaylistIndex()
+
+    // use JS destructuring syntax to exclude the `description` property from the rest of the object, it can be too long
+    const { description, ...destructuredVideo } = props.videos[currentVideoIndex]
+    const errorMessage = `BROKEN VIDEO: ${videoId}, videoUrl: ${videoUrl}, video: ${JSON.stringify(destructuredVideo)}`
+
+    console.log(errorMessage)
+    Honeybadger.notify(errorMessage);
+
+    if (currentVideoIndex === 0) {
+      console.log("Recovering from broken first video...")
+      props.onErrorRecovery()
+    } else {
+      internalPlayer.nextVideo()
+    }
   }
 
   function getVideoId(url) {
--- src/hooks/UseVideoPlayer.js
+++ src/hooks/UseVideoPlayer.js
@@ -3,7 +3,7 @@ import useVideoDataFetcher from '../hooks/UseVideoDataFetcher'
 
 export default function useVideoPlayer(selectedPlaylistIds) {
     const [currentVideos, setCurrentVideos] = useState([])
-    const [videoFetchResult, setVideoFetchPlaylistIds] = useVideoDataFetcher(selectedPlaylistIds)
+    const [videoFetchResult, setVideoFetchPlaylistIds, triggerRefetch] = useVideoDataFetcher(selectedPlaylistIds)
 
     useEffect(shuffleVideos, [videoFetchResult])
 
@@ -27,6 +27,7 @@ export default function useVideoPlayer(selectedPlaylistIds) {
     return {
         currentVideos,
         setVideoFetchPlaylistIds,
+        triggerRefetch,
         isLoaded: videoFetchResult.isLoaded
     }
 }
