71d7326da9ffc798c1066216f161c653d1d7b8d6
Robert Skinner <ras2@live.com>
2024-08-31 21:40:01 -0400

    Add video information to the error message

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,22 +6,27 @@ export default function Player(props) {
   const YOUTUBE_PLAYLIST_VIDEO_LIMIT = 200
 
   function onPlay() {
-    var internalPlayer = props.playerRef.current?.getInternalPlayer()
-    var videoData = internalPlayer.getVideoData()
-    var video = props.videos.find(v => v.video_id === videoData.video_id)
+    const internalPlayer = props.playerRef.current?.getInternalPlayer()
+    const videoData = internalPlayer.getVideoData()
+    const video = props.videos.find(v => v.video_id === videoData.video_id)
     props.setCurrentVideo(video)
   }
 
   function onError() {
-    var internalPlayer = props.playerRef.current?.getInternalPlayer()
-    var videoUrl = internalPlayer.getVideoUrl()
+    const internalPlayer = props.playerRef.current?.getInternalPlayer()
+    const videoUrl = internalPlayer.getVideoUrl()
     if (!videoUrl) return
 
-    var videoId = getVideoId(videoUrl)
-    console.log(`BROKEN VIDEO: ${videoId}, videoUrl: ${videoUrl}`)
-    Honeybadger.notify(`BROKEN VIDEO: ${videoId}, videoUrl: ${videoUrl}`);
+    const videoId = getVideoId(videoUrl)
+    const currentVideoIndex = internalPlayer.getPlaylistIndex()
 
-    var currentVideoIndex = internalPlayer.getPlaylistIndex()
+    // use JS destructuring syntax to exclude the `description` property from the rest of the object, it can be too long
+    const { description, ...destructuredVideo } = props.videos[currentVideoIndex]
+    const errorMessage = `BROKEN VIDEO: ${videoId}, videoUrl: ${videoUrl}, video: ${JSON.stringify(destructuredVideo)}`
+
+    console.log(errorMessage)
+    Honeybadger.notify(errorMessage);
+
     if (currentVideoIndex === 0) {
       console.log("Recovering from broken first video...")
       props.onErrorRecovery()
