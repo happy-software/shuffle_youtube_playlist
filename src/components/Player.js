770ff6076b8f2944f1c9919d9d7678b0217dae22
Robert Skinner <ras2@live.com>
2024-09-02 21:14:18 -0400

    add honeybadger context

--- src/components/Player.js
+++ src/components/Player.js
@@ -12,29 +12,28 @@ export default function Player(props) {
     props.setCurrentVideo(video)
   }
 
-  function onError() {
+  async function onError() {
     const internalPlayer = props.playerRef.current?.getInternalPlayer()
     const videoUrl = internalPlayer.getVideoUrl()
-    const videoId = getVideoId(videoUrl)
 
     const currentVideoIndex = internalPlayer.getPlaylistIndex()
     // use JS destructuring syntax to exclude the `description` property from the rest of the object, it can be too long
-    const { description, ...destructuredVideo } = props.videos[currentVideoIndex]
+    const { description, ...errorVideo } = props.videos[currentVideoIndex]
+    errorVideo.videoIndex = currentVideoIndex
 
-    const errorMessage = `BROKEN VIDEO: ${videoId}, index: ${currentVideoIndex}, videoUrl: ${videoUrl}, video: ${JSON.stringify(destructuredVideo)}`
-    console.log(errorMessage)
-    Honeybadger.notify(errorMessage);
+    console.log('BROKEN VIDEO')
+    await Honeybadger.notifyAsync({
+      message: `BROKEN VIDEO: ${errorVideo.title}`,
+      context: {
+        ...errorVideo
+      }
+    })
 
     if (currentVideoIndex === 0 || !videoUrl) {
       window.location.reload()
     } else {
       internalPlayer.nextVideo()
     }
-  }
-
-  function getVideoId(url) {
-    const urlParams = new URLSearchParams(new URL(url).search)
-    return urlParams.get('v')
   }
 
   return <div className='playerWrapper' style={{ display: props.hideVideo ? 'none' : 'block' }}>
